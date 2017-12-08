a8dcba133f70dd80ab176eb8b5c027994c3980ce
Thomson <779387850@qq.com>
2017-12-08 14:44:23 +0800

    计数器记录arguments长度

--- 编程练习题/柯里化.js
+++ 编程练习题/柯里化.js
@@ -2,18 +2,22 @@
 // 具体见示例（这是 lodash.curry 的文档示例
 //https://yi-love.github.io/blog/javascript/2016/12/08/js-func-curry.html
 function curry (fn, thisArg) {
+  let length = fn.length
   if (!Array.isArray(thisArg)) {
     thisArg = []
   }
   return function () {
     let args = Array.prototype.slice.call(arguments)
-    if ((args.length + thisArg.length) < fn.length) {
+    // 已经添加的参数数量小于预期数量,继续递归
+    if ((args.length + thisArg.length) < length) {
       return curry(fn, thisArg.concat(args))
     }
     return fn.apply(this, thisArg.concat(args))
   }
 }
 
+
+// 测试代码
 let abc = function (a, b, c) {
   return [a, b, c]
 }
