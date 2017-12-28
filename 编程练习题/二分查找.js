af92b29933b0293716d4683dc6e5a5bfb0455fef
Thomson <779387850@qq.com>
2017-12-28 14:41:39 +0800

    二分查找

---
+++ 编程练习题/二分查找.js
@@ -0,0 +1,22 @@
+/**
+ * 二分查找，递归实现。
+ * @param target
+ * @param arr
+ * @param start
+ * @param end
+ * @returns {*}
+ */
+function binarySearch (target, arr, start, end) {
+  let start = start || 0
+  let end = end || arr.length - 1
+
+  let mid = parseInt(start + (end - start) / 2)
+  if (target === arr[mid]) {
+    return mid
+  } else if (target > arr[mid]) {
+    return binarySearch(target, arr, mid + 1, end)
+  } else {
+    return binarySearch(target, arr, start, mid - 1)
+  }
+  return -1
+}
