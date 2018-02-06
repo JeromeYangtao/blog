8399e5865ca2f9a7593db59e2751ad12765737a1
Thomson <779387850@qq.com>
2018-02-06 22:49:26 +0800

    归并排序

--- 编程练习题/归并排序.js
+++ 编程练习题/归并排序.js
@@ -19,3 +19,22 @@ function mergeSort(arr) {
 
   return merge(mergeSort(left), mergeSort(right))
 }
+
+// 不用递归
+// function mergeSort(a) {
+//   if (a.length === 1) return a
+
+//   var work = []
+//   for (var i = 0, len = a.length; i < len; i++) work.push([a[i]])
+
+//   work.push([]) // 如果数组长度为奇数
+
+//   for (var lim = len; lim > 1; lim = ~~((lim + 1) / 2)) {
+//     for (var j = 0, k = 0; k < lim; j++, k += 2)
+//       work[j] = merge(work[k], work[k + 1])
+
+//     work[j] = [] // 如果数组长度为奇数
+//   }
+
+//   return work[0]
+// }
