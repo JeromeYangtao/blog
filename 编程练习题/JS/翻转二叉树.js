2b1971b86e1c39f4cec1b06b087656cc49c8e31a
Thomson <779387850@qq.com>
2018-03-27 23:06:07 +0800

    翻转二叉树

---
+++ 编程练习题/JS/翻转二叉树.js
@@ -0,0 +1,15 @@
+var invertTree = function(root) {
+  if(root == null) { // 如果不存在，则返回[]
+      return root;
+  } else { // 交换左右结点
+      var temp = root.left;
+      root.left  = root.right;
+      root.right = temp;
+  }
+  invertTree(root.left); // 转换左子树
+  invertTree(root.right); // 转换右子树
+  return root;//返回最后结果
+};
+
+参考:
+http://binjs.com/2017/06/01/JS%E5%8F%8D%E8%BD%AC%E4%BA%8C%E5%8F%89%E6%A0%91/
