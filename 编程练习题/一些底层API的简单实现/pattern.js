efb60fa73fa965da634b0d5770e35ad61e2c9dce
Thomson <779387850@qq.com>
2018-04-03 21:45:52 +0800

    bind的实现

--- 编程练习题/一些底层API的简单实现/pattern.js
+++ 编程练习题/一些底层API的简单实现/pattern.js
@@ -1,5 +1,9 @@
-function flatten(arr) {
+let arr = [1, 2, 3, [4, 5, [6, 7]]]
+
+function flatten (arr) {
   return arr.reduce(function (pre, next) {
-      return pre.concat(Array.isArray(next) ? flatten(next) : next)
+    return pre.concat(Array.isArray(next) ? flatten(next) : next)
   }, [])
 }
+
+console.log(flatten(arr))
