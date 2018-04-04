56c0e46f26fda2da4704dd8737cb02b1574080a2
Thomson <779387850@qq.com>
2018-04-04 08:51:03 +0800

    调整目录结构

--- 编程练习题/一些底层API的简单实现/bind.js
+++ 编程练习题/一些底层API的简单实现/bind.js
@@ -5,14 +5,11 @@ function bind (fn, obj) {
 }
 
 Function.prototype.bind2 = function (context) {
-
   if (typeof this !== 'function') {
     throw new Error('Function.prototype.bind - what is trying to be bound is not callable')
   }
-
   var self = this
   var args = Array.prototype.slice.call(arguments, 1)
-
   var fNOP = function () {}
 
   var fBound = function () {
