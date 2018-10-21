76a63b2f8bede54d42f1e2e62603c900628f4f74
Thomson <yangtao135@foxmail.com>
2018-10-21 10:27:41 +0800

    重构isValid

--- 编程练习题/Java/src/com/study/Main.java
+++ 编程练习题/Java/src/com/study/Main.java
@@ -1,16 +1,14 @@
 package com.study;
 
-import java.util.HashMap;
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Solution solution = new Solution();
-        System.out.println(solution.isValid("{()}"));
-        System.out.println(solution.isValid(""));
-        System.out.println(solution.isValid("{[]{"));
-        System.out.println(solution.isValid("{}{()[]]"));
-        System.out.println(solution.isValid("{}[]()"));
+        IsValid solution = new IsValid();
+        System.out.println(solution.solution("{()}"));
+        System.out.println(solution.solution(""));
+        System.out.println(solution.solution("{[]{"));
+        System.out.println(solution.solution("{}{()[]]"));
+        System.out.println(solution.solution("{}[]()"));
     }
 }
--- 编程练习题/Java/src/com/study/isValid.java
+++ 编程练习题/Java/src/com/study/isValid.java
@@ -6,32 +6,32 @@ import java.util.Stack;
 
 //给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
 
-public class isValid {
-    public boolean isValid(String s) {
-        String[] left = {"{", "[", "("};
-        HashMap<String, String> map = new HashMap<>();
-        map.put("(", ")");
-        map.put("{", "}");
-        map.put("[", "]");
-        Stack<String> stack = new Stack<>();
+public class IsValid {
+    private HashMap<Character, Character> map;
+
+    public IsValid() {
+        this.map = new HashMap<>();
+        this.map.put('(', ')');
+        this.map.put('{', '}');
+        this.map.put('[', ']');
+    }
+
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            String node = String.valueOf(s.toCharArray()[i]);
-            if (Arrays.asList(left).contains(node)) {
+            char node = s.charAt(i);
+            if (this.map.containsKey(node)) {
                 stack.push(node);
             } else {
-                if(stack.empty()){
+                if (stack.empty()) {
                     return false;
                 }
-                String pop = stack.pop();
-                String test = map.get(pop);
-                if (!test.equals(node)) {
+                char pop = stack.pop();
+                if (this.map.get(pop) != node) {
                     return false;
                 }
             }
         }
-        if (!stack.empty()) {
-            return false;
-        }
-        return true;
+        return stack.empty();
     }
 }
