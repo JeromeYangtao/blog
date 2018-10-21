948b8285b1080e57ecfd9070816d56e7e80f21bd
Thomson <yangtao135@foxmail.com>
2018-10-21 20:04:19 +0800

    刷题:只出现一次的数

--- 编程练习题/Java/src/com/study/Main.java
+++ 编程练习题/Java/src/com/study/Main.java
@@ -3,12 +3,17 @@ package com.study;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        IsValid solution = new IsValid();
-        System.out.println(solution.solution("{()}"));
-        System.out.println(solution.solution(""));
-        System.out.println(solution.solution("{[]{"));
-        System.out.println(solution.solution("{}{()[]]"));
-        System.out.println(solution.solution("{}[]()"));
+        int[] arr = new int[]{5, 4, 8, 6, 3, 9, 0, 1, 7, 2};
+        Solution solution = new Solution();
+        solution.quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
     }
+
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
--- 编程练习题/Java/src/com/study/Solution.java
+++ 编程练习题/Java/src/com/study/Solution.java
@@ -5,5 +5,45 @@ import java.util.HashMap;
 import java.util.Stack;
 
 public class Solution {
+    //    快排
+    void quickSort(int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int pivot = arr[start];
+        int low = start;
+        int height = end;
+        while (low < height) {
+            while (low < height && arr[height] >= pivot) {
+                height--;
+            }
+            arr[low] = arr[height];
+            while (low < height && arr[low] <= pivot) {
+                low++;
+            }
+            arr[height] = arr[low];
+        }
+        arr[low] = pivot;
+        quickSort(arr, start, low - 1);
+        quickSort(arr, low + 1, end);
+    }
 
+    //136. 只出现一次的数字
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for (int num : nums) {
+            if (numMap.containsKey(num)) {
+                numMap.put(num, 2);
+            } else {
+                numMap.put(num, 1);
+            }
+        }
+
+        for (int num : numMap.keySet()) {
+            if (numMap.get(num) == 1) {
+                return num;
+            }
+        }
+        return 0;
+    }
 }
